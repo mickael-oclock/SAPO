59cb248b8bc2567a867e51d57c04e29bde34c3b4
Mickael De PECKER <mickael@oclock.io>
2020-07-09 15:15:21 +0200

    add my lovely Extended controller

---
+++ src/Controller/ExtendedController.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class ExtendedController extends AbstractController
+{
+
+    protected $context;
+    protected $translator;
+
+    private const SUCCESS_FLASH_KEY = "success";
+    private const WARNING_FLASH_KEY = "warning";
+    private const ERROR_FLASH_KEY = "error";
+    public function __construct( TranslatorInterface $translator)
+    {
+        $this->context = [];
+        $this->translator = $translator;
+    }
+    protected function   addToContext($key,$value){
+        $this->context[$key] = $value;
+    }
+    protected function addSuccessFlash($translatableKey){
+        $this->addFlash(self::SUCCESS_FLASH_KEY,$this->translator->trans($translatableKey));
+    }
+    protected function addErrorFlash($translatableKey){
+        $this->addFlash(self::ERROR_FLASH_KEY,$this->translator->trans($translatableKey));
+    }
+    protected function addWarningFlash($translatableKey){
+        $this->addFlash(self::WARNING_FLASH_KEY,$this->translator->trans($translatableKey));
+    }
+    protected function renderWithContext(string $view, Response $response = null): Response
+    {
+        return parent::render($view, $this->context, $response);
+    }
+}
